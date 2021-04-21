0aac6549badfbe59592fa7739e206f949666fb7e
Neyffer Palacios <tonyffer7@hotmail.com>
2021-04-21 02:25:51 -0500

    Found the problem: MongoDB just hace connections from my own IP

--- config/index.js
+++ config/index.js
@@ -5,4 +5,6 @@ if (process.env.NODE_ENV !== "production") {
 module.exports = {
 	PORT: process.env.PORT,
 	MONGO_URI: process.env.MONGO_URI,
+	USER_EMAIL: process.env.USER_EMAIL,
+	USER_PASSWORD: process.env.USER_PASSWORD,
 };
