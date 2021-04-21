0964f1f41219115e7f4861435b1c69b7726f78bd
Neyffer Palacios <tonyffer7@hotmail.com>
2021-04-21 00:18:59 -0500

    Example with data for -Equipos-

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
+const importData = require("./data.json");
+const cors = require ('cors'); // Conectar servidor del backend con el del frontend
 let port = process.env.PORT || 3001;
+
+
+
+// ***Middlewares***
+app.use(cors({origin: 'http://localhost:4200'}));	
 
 
 app.get("/", (req, res)=>{
     res.send("Hello Word from JGestons RTR")
 });
+
+app.get("/api/equipo/obtener", (req, res)=>{
+    res.send(importData);
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app is listening on port http://localhost:${port}`);
