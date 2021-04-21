8e11301febc6eefb2570bbff475401dde031e7c2
Neyffer Palacios <tonyffer7@hotmail.com>
2021-04-21 01:17:13 -0500

    Deleted the variables from config file

--- config/index.js
+++ config/index.js
@@ -1,10 +1,11 @@
 if (process.env.NODE_ENV !== "production") {
-	require("dotenv").config();
+//	require("dotenv").config();
 }
 
+let PORT = process.env.PORT || 30001;
+
 module.exports = {
-	PORT: process.env.PORT,
-	MONGO_URI: process.env.MONGO_URI,
-	USER_EMAIL: process.env.USER_EMAIL,
-	USER_PASSWORD: process.env.USER_PASSWORD,
+	PORT,
+	//USER_EMAIL: process.env.USER_EMAIL,
+	//USER_PASSWORD: process.env.USER_PASSWORD,
 };
